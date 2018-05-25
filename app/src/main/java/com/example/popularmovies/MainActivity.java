58b8fac5363de8fb328c4e0f724591b3741aa9ed
Ambar Kumar <ambar.ara123@gmail.com>
2018-05-25 12:49:53 +0530

    sort menu added

--- app/src/main/java/com/example/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/popularmovies/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static TextView NEWText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+
+    //doing sort menu related stuff
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflating menu item
+        getMenuInflater().inflate(R.menu.sort,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //getting id of menu items
+        int id = item.getItemId();
+
+        if (id == R.id.action_popular){
+            //popular sort coding will done here
+            return true;
+        }else if (id== R.id.acrion_highestrated){
+            //highest rated coding will be done here
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
